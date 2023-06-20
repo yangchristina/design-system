6d52718d8dc84697f2a35cbb5676360c6f87c594
Christina Yang <ychristina011@gmail.com>
2023-06-19 18:54:43 -0700

    try upgrade, may rollback

---
+++ rollup.config.mjs
@@ -0,0 +1,31 @@
+// import typescript from 'rollup-plugin-typescript2';
+import pkg from './package.json' assert { type: 'json' };
+import typescript from '@rollup/plugin-typescript';
+
+export default {
+  input: './index.ts',
+  output: {
+    dir: 'dist',
+  },
+  // [
+  //   {
+  //     file: pkg.main,
+  //     format: 'cjs',
+  //     banner: "'use client'",
+  //   },
+  //   {
+  //     file: pkg.module,
+  //     format: 'es',
+  //     banner: "'use client'",
+  //   },
+  // ],
+  external: [...Object.keys(pkg.dependencies || {}), ...Object.keys(pkg.peerDependencies || {})],
+  plugins: [
+    typescript(),
+    // typescript({
+    //   clean: true,
+    //   tsconfig: 'tsconfig.json',
+    //   typescript: import('typescript'),
+    // }),
+  ],
+};
