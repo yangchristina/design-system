82ec0d746b22660b45b3612d87945d13df3f9a8c
Christina Yang <ychristina011@gmail.com>
2023-06-23 23:13:18 -0700

    zIndex sheet content

--- components/Sheet.tsx
+++ components/Sheet.tsx
@@ -53,6 +53,7 @@ const StyledContent = styled(DialogPrimitive.Content, {
   top: 0,
   bottom: 0,
   width: 250,
+  zIndex: '$3',
 
   // Among other things, prevents text alignment inconsistencies when dialog can't be centered in the viewport evenly.
   // Affects animated and non-animated dialogs alike.
