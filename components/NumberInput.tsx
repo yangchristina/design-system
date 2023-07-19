2c28f4aad029e79eae7c4d711e36fbd01337411e
Christina Yang <ychristina011@gmail.com>
2023-07-19 00:06:47 -0700

    number input nullable

--- components/NumberInput.tsx
+++ components/NumberInput.tsx
@@ -7,16 +7,28 @@ import { Input } from './Input'
 import { round } from 'lodash'
 
 type InputProps = Omit<React.InputHTMLAttributes<HTMLInputElement>, 'type' | 'min' | 'max' | 'onChange' | 'value'> & {
-    error?: boolean, label?: string, max?: number, min?: number, handleChange: (value: number) => void, value?: number, precision?: number, integerOnly?: boolean
-}
+    error?: boolean, label?: string, max?: number, min?: number,
+    value?: number, precision?: number, integerOnly?: boolean,
+} & ({
+    nullable: true,
+    handleChange: (value: number | null) => void
+} | {
+    nullable?: false,
+    handleChange: (value: number) => void
+})
 // TODO: allow decimal values, what does size even do??? not in use currently
-export const NumberInput = forwardRef<HTMLInputElement, InputProps>(({ children, label, error, handleChange, value, id, min, max, precision, integerOnly, size, ...props }, forwardedRef) => {
+export const NumberInput = forwardRef<HTMLInputElement, InputProps>(({ children, label, error, handleChange, value, id, min, max, precision, integerOnly, size, nullable, ...props }, forwardedRef) => {
     const [state, setState, revert] = useEdit<number | string>(value ?? '')
     if (integerOnly) {
         precision = 0
     }
     const handleChangeDebounced = (e: ChangeEvent<HTMLInputElement>) => {
         const int = precision !== undefined ? round(parseFloat(e.target.value), precision) : parseFloat(e.target.value)
+        if (nullable && !e.target.value) {
+            // @ts-expect-error
+            handleChange(null)
+            return
+        }
         if (max !== undefined && int > max) {
             handleChange?.(max)
             return
