7515f71ddb0c765e6f294b19ce8267947a5bca27
Christina Yang <ychristina011@gmail.com>
2023-08-14 10:53:01 -0700

    add solid variant

--- components/Input.tsx
+++ components/Input.tsx
@@ -59,6 +59,9 @@ export const InputField = styled('input', {
             invisible: {
                 boxShadow: 'none',
                 backgroundColor: 'inherit',
+            },
+            solid: {
+                backgroundColor: '$loContrast',
             }
         }
     }
