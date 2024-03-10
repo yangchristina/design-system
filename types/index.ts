bd27faed1d5ba08e91f841dc7e7f21b5b351abbb
Christina Yang <ychristina011@gmail.com>
2024-03-10 14:54:20 -0700

    add type

--- types/index.ts
+++ types/index.ts
@@ -1,3 +1,9 @@
 export type UnionOmit<T, K extends string | number | symbol> = T extends unknown
     ? Omit<T, K>
     : never
+
+export type DayOfWeekNum = 0 | 1 | 2 | 3 | 4 | 5 | 6
+
+export type Nullable<T> = {
+    [P in keyof T]: T[P] | null;
+};
