d9fa0c5ac4a9c211b444931fd493d881f8b7c891
Christina Yang <ychristina011@gmail.com>
2024-03-11 10:06:12 -0700

    add date helper

--- utils/date.ts
+++ utils/date.ts
@@ -3,3 +3,8 @@ import { format } from "date-fns"
 export function getDayStringFormat(date: Date | number) {
     return format(date, 'yyyy-MM-dd')
 }
+
+export function dayStringToLocalDate(isoString: string) {
+    const [year, month, day] = isoString.split('-');
+    return new Date(parseInt(year), parseInt(month) - 1, parseInt(day), 0, 0, 0);
+}
