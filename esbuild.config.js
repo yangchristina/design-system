35b18643a47aa02a986bb2d5b4be5d4a338a0dab
Christina Yang <ychristina011@gmail.com>
2024-08-02 18:34:08 -0700

    fix: output dir back to dist

--- esbuild.config.js
+++ esbuild.config.js
@@ -8,7 +8,7 @@ const CssModulesPlugin = require('esbuild-css-modules-plugin');
 esbuild
   .build({
     entryPoints: ['src/index.ts'],
-    outdir: 'build',
+    outdir: 'dist',
     bundle: true,
     minify: false,
     treeShaking: true,
