cb8c59d958303b407ff58fb8eea383dd804b0c15
Christina Yang <ychristina011@gmail.com>
2024-08-26 13:34:33 -0700

    remove kbds, convert simple toggle

--- src/components/SimpleToggle.tsx
+++ src/components/SimpleToggle.tsx
@@ -1,59 +1,56 @@
 "use client"
-import { styled } from '../stitches.config';
+import { styled } from '@planda/styled-system/jsx';
 import * as TogglePrimitive from '@radix-ui/react-toggle';
 
 export const SimpleToggle = styled(TogglePrimitive.Root, {
-  // Reset
-  alignItems: 'center',
-  appearance: 'none',
-  borderWidth: '0',
-  boxSizing: 'border-box',
-  display: 'inline-flex',
-  flexShrink: 0,
-  fontFamily: 'inherit',
-  fontSize: '14px',
-  justifyContent: 'center',
-  lineHeight: '1',
-  outline: 'none',
-  padding: '0',
-  textDecoration: 'none',
-  userSelect: 'none',
-  WebkitTapHighlightColor: 'transparent',
-  color: '$hiContrast',
-  '&::before': {
+  base: {
+    // Reset
+    alignItems: 'center',
+    appearance: 'none',
+    borderWidth: '0',
     boxSizing: 'border-box',
-  },
-  '&::after': {
-    boxSizing: 'border-box',
-  },
-  height: '$5',
-  width: '$5',
-  backgroundColor: 'transparent',
-  '@hover': {
+    display: 'inline-flex',
+    flexShrink: 0,
+    fontFamily: 'inherit',
+    fontSize: '14px',
+    justifyContent: 'center',
+    lineHeight: '1',
+    outline: 'none',
+    padding: '0',
+    textDecoration: 'none',
+    userSelect: 'none',
+    WebkitTapHighlightColor: 'transparent',
+    color: '$hiContrast',
+    '&::before': {
+      boxSizing: 'border-box',
+    },
+    '&::after': {
+      boxSizing: 'border-box',
+    },
+    height: '$5',
+    width: '$5',
+    backgroundColor: 'transparent',
     '&:hover': {
       backgroundColor: '$grayA3',
     },
-  },
-  '&:active': {
-    backgroundColor: '$grayA4',
-  },
-  '&:focus': {
-    boxShadow: 'inset 0 0 0 1px $grayA8, 0 0 0 1px $grayA8',
-    zIndex: 1,
-  },
+    '&:active': {
+      backgroundColor: '$grayA4',
+    },
+    '&:focus': {
+      boxShadow: 'inset 0 0 0 1px $grayA8, 0 0 0 1px $grayA8',
+      zIndex: 1,
+    },
 
-  '&[data-state="on"]': {
-    backgroundColor: '$grayA5',
-    '@hover': {
+    '&[data-state="on"]': {
+      backgroundColor: '$grayA5',
       '&:hover': {
         backgroundColor: '$grayA5',
       },
-    },
-    '&:active': {
-      backgroundColor: '$grayA7',
+      '&:active': {
+        backgroundColor: '$grayA7',
+      },
     },
   },
-
   variants: {
     shape: {
       circle: {
