20a692943cb22600806b8db40225a3989641d729
Christina Yang <ychristina011@gmail.com>
2024-08-26 13:31:43 -0700

    remove unused, remove next-themes

--- src/components/DesignSystemProvider.tsx
+++ src/components/DesignSystemProvider.tsx
@@ -1,8 +1,6 @@
 "use client"
 import React from 'react';
 import { TooltipProvider } from '@radix-ui/react-tooltip';
-import { ThemeProvider } from 'next-themes';
-import { providerThemes } from "../stitches.config";
 // import '@radix-ui/themes/styles.css';
 // import Theme from './Theme';
 
@@ -26,12 +24,7 @@ const mergeThemes = (themes1: Record<string, string>, themes2: Record<string, st
 }
 
 export const DesignSystemProvider: React.FC<DesignSystemProviderProps> = ({ themes, defaultTheme = 'teal', enableSystem, ...props }) => {
-  return <ThemeProvider
-    attribute="class"
-    defaultTheme={defaultTheme} // normally leave as system
-    enableSystem={enableSystem}
-    value={{ ...providerThemes, ...themes, }}
-  ><TooltipProvider {...props} /></ThemeProvider >;
+  return <TooltipProvider {...props} />
 };
 
 export const DesignSystemTooltipOnlyProvider: React.FC<DesignSystemProviderProps> = (props) => {
