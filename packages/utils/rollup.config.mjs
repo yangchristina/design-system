1842b6d3ee92cee05dd0d4a1e4dc06648ea3ac16
Christina Yang <ychristina011@gmail.com>
2024-08-31 15:19:02 -0700

    add utils package

---
+++ packages/utils/rollup.config.mjs
@@ -0,0 +1,54 @@
+// import typescript from 'rollup-plugin-typescript2';
+// If using node-22, use with instead of assert
+import pkg from './package.json' with { type: 'json' };
+import typescript from '@rollup/plugin-typescript';
+import preserveDirectives from 'rollup-plugin-preserve-directives';
+// import commonjs from '@rollup/plugin-commonjs';
+import resolve from '@rollup/plugin-node-resolve';
+// import babel from '@rollup/plugin-babel';
+import terser from '@rollup/plugin-terser';
+
+const plugins = [
+  resolve(),
+  // typescript({
+    terser({ sourceMap: true }),
+  // }),
+  preserveDirectives(),
+]
+
+const commonConfig = {
+  input: './src/index.ts',
+  external: [...Object.keys(pkg.dependencies || {}), ...Object.keys(pkg.peerDependencies || {})],
+
+}
+
+export default [
+  {
+    ...commonConfig,
+    output: [
+      {
+      dir: 'dist',
+      format: 'esm',
+      exports: 'named',
+      sourcemap: true,
+      preserveModules: true,
+      preserveModulesRoot: 'src',
+      minifyInternalExports: true,
+      }
+    ],
+    plugins: [typescript({ declaration: true }), ...plugins]
+  },
+  {
+    ...commonConfig,
+    output: [
+      {
+      file: pkg.main,
+      format: 'cjs',
+      exports: 'named',
+      sourcemap: true,
+      minifyInternalExports: true,
+      }
+    ],
+    plugins: [typescript({ declaration: false }), ...plugins]
+  }
+];
---
+++ packages/utils/src/index.ts
@@ -0,0 +1,7 @@
+export * from './math';
+export * from './utils';
+export * from './date';
+export * from './cast';
+export * from './json';
+export * from './fetch';
+export * from './constants';
