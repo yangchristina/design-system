417fb8e3bd96a0a42aaa017a763b93b101ab9c41
Christina Yang <ychristina011@gmail.com>
2024-10-30 23:13:40 -0700

    fix types

--- packages/design-system/src/createStyleContext.tsx
+++ packages/design-system/src/createStyleContext.tsx
@@ -1,56 +1,47 @@
-import React, { createContext, forwardRef, useContext, type ComponentProps, type ElementType } from 'react'
-type Props = Record<string, unknown>
+import React, { createContext, forwardRef, useContext, type ComponentProps, type ElementType } from 'react';
+import { Override } from './types';
+type Props = Record<string, unknown>;
 type Recipe = {
-    (props?: Props): Record<string, string>
-    splitVariantProps: (props: Props) => any
-}
-type Slot<R extends Recipe> = keyof ReturnType<R>
-type SlotRecipe<R extends Recipe> = Record<Slot<R>, string>
-type VariantProps<R extends Recipe> = Parameters<R>[0]
+    (props?: Props): Record<string, string>;
+    splitVariantProps: (props: Props) => any;
+};
+type Slot<R extends Recipe> = keyof ReturnType<R>;
+type SlotRecipe<R extends Recipe> = Record<Slot<R>, string>;
+type VariantProps<R extends Recipe> = Parameters<R>[0];
 
-const cx = (...args: (string | undefined)[]) => args.filter(Boolean).join(' ')
+const cx = (...args: (string | undefined)[]) => args.filter(Boolean).join(' ');
 
 export const createStyleContext = <R extends Recipe>(recipe: R) => {
-    const StyleContext = createContext<SlotRecipe<R> | null>(null)
+    const StyleContext = createContext<SlotRecipe<R> | null>(null);
 
-    const withProvider = <C extends ElementType>(Component: C, slot?: Slot<R>): C => {
-        const Comp = forwardRef((props: ComponentProps<C> & VariantProps<R>, ref) => {
-            const [variantProps, localProps] = recipe.splitVariantProps(props)
-            const slotRecipe = recipe(variantProps) as SlotRecipe<R>
+    const withProvider = <C extends ElementType>(Component: C, slot?: Slot<R>) => {
+        const Comp = forwardRef<any, Override<ComponentProps<C>, NonNullable<VariantProps<R>>>>((props, ref) => {
+            const [variantProps, localProps] = recipe.splitVariantProps(props);
+            const slotRecipe = recipe(variantProps) as SlotRecipe<R>;
             return (
                 <StyleContext.Provider value={slotRecipe}>
-                    <Component
-                        ref={ref}
-                        {...(props as any)}
-                        className={cx(slotRecipe[slot ?? ''], localProps.className)}
-                    />
+                    <Component ref={ref} {...(props as any)} className={cx(slotRecipe[slot ?? ''], localProps.className)} />
                 </StyleContext.Provider>
-            )
-        })
+            );
+        });
         // @ts-expect-error JSX.IntrinsicElements do not have a displayName but Function and Class components do
-        Comp.displayName = Component.displayName || Component.name || 'Component'
-        return Comp as unknown as C
-    }
+        Comp.displayName = Component.displayName || Component.name || 'Component';
+        return Comp;
+    };
 
     const withContext = <C extends ElementType>(Component: C, slot?: Slot<R>): C => {
         const Comp = forwardRef((props: ComponentProps<C>, ref) => {
-            const recipe = useContext(StyleContext)
-            return (
-                <Component
-                    ref={ref}
-                    {...(props as any)}
-                    className={cx(recipe?.[slot ?? ''], props.className)}
-                />
-            )
-        })
+            const recipe = useContext(StyleContext);
+            return <Component ref={ref} {...(props as any)} className={cx(recipe?.[slot ?? ''], props.className)} />;
+        });
 
         // @ts-expect-error JSX.IntrinsicElements do not have a displayName but Function and Class components do
-        Comp.displayName = Component.displayName || Component.name || 'Component'
-        return Comp as unknown as C
-    }
+        Comp.displayName = Component.displayName || Component.name || 'Component';
+        return Comp as unknown as C;
+    };
 
     return {
         withProvider,
         withContext,
-    }
-}
+    };
+};
