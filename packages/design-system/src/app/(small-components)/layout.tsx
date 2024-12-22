04a1aa96505a1aba8699fda12204158d2296790a
Christina Yang <ychristina011@gmail.com>
2024-12-21 21:15:12 -0800

    setup testing pages

---
+++ packages/design-system/src/app/(small-components)/layout.tsx
@@ -0,0 +1,9 @@
+'use client'
+
+export default function Layout({children}: {children: React.ReactNode}) {
+    return (
+        <section style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', height: '100vh', width: '100vw' }}>
+            {children}
+        </section>
+    );
+}
