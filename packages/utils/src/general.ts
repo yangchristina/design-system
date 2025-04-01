477af993a41c53d889c7992cd2170848e9878d22
Christina Yang <ychristina011@gmail.com>
2025-04-01 09:17:09 -0700

    refactor: move dependencies to peerDependencies and improve utility functions

--- packages/utils/src/general.ts
+++ packages/utils/src/general.ts
@@ -1,28 +1,34 @@
+import { Truthy } from 'lodash';
+
 // not in use (might not work)
 export function getValuesRecursively<T>(obj?: Record<string, any>): T[] | undefined {
-    if (!obj) return obj
-    let arr: T[] = []
+    if (!obj) return obj;
+    let arr: T[] = [];
     for (let key in obj) {
-        if (typeof obj[key] === "object") {
-            const res = getValuesRecursively<any>(obj[key])
-            if (!res) continue
-            arr = arr.concat(res)
+        if (typeof obj[key] === 'object') {
+            const res = getValuesRecursively<any>(obj[key]);
+            if (!res) continue;
+            arr = arr.concat(res);
         } else {
-            arr.push(obj[key])
+            arr.push(obj[key]);
         }
     }
-    return arr
+    return arr;
 }
 
 export const removeUndefinedOrNull = <T>(obj: T) => {
     // @ts-expect-error
-    const newObj: T = {}
+    const newObj: T = {};
 
     // pick(obj, Object.keys(obj).filter(k=>obj[k] !== undefined && obj[k]))
     for (let key in obj) {
-        const val = obj[key]
-        if (obj[key] === undefined || obj[key] === null) continue
-        newObj[key] = val
+        const val = obj[key];
+        if (obj[key] === undefined || obj[key] === null) continue;
+        newObj[key] = val;
     }
-    return newObj
+    return newObj;
+};
+
+export function isTruthy<T>(value: T): value is Truthy<T> {
+    return !!value;
 }
