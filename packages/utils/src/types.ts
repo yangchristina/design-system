428af256a3a0a7b119871e18a6bb7b4144538ee4
Christina Yang <ychristina011@gmail.com>
2025-04-07 07:40:37 -0700

    feat: add new type definitions for MaybePromise, NextJSPageProps, and ValueOf

--- packages/utils/src/types.ts
+++ packages/utils/src/types.ts
@@ -1,1 +1,10 @@
 export type DayOfWeekNum = 0 | 1 | 2 | 3 | 4 | 5 | 6;
+
+export type MaybePromise<T> = T | Promise<T>;
+
+export type NextJSPageProps<P> = {
+    params: Promise<P>;
+    searchParams: Promise<{ [key: string]: string | string[] | undefined }>;
+};
+
+export type ValueOf<T> = T[keyof T];
